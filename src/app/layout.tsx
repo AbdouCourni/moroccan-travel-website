9218ce91e06a69343b20e26f6d90739e94dafde9
abdou errakhaoui <abdouerra2011@gmail.com>
2025-10-19 00:56:44 +0100

    logo search result added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -156,6 +156,7 @@ export default async function RootLayout({
                 addressCountry: 'MA',
                 addressLocality: 'Marrakech',
               },
+              "logo": "https://www.morocompase.com/apple-touch-icon.png",
               areaServed: 'Morocco',
               offers: {
                 '@type': 'Offer',
